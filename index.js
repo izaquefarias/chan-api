99bb077183d74b64b0d2657b24cd11e8214f2237
izaquefarias <zac_farias@hotmail.com>
2022-06-22 09:41:35 -0300

    conclusão da infraestrutura e criacao do arquivo queries

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/database');
 const Tabelas = require('./infraestrutura/database/tabelas');
+const port = 3003;
+
+conexao.connect((erro)=>{
+    if(erro){
+        console.log(erro);
+    }else{
+        Tabelas.init(conexao);
+        
+        const app = customExpress();
+
+        app.listen(port, () => console.log(`Servidor iniciado na porta: ${port}`));
+    };
+});
